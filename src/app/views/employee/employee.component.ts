b970fc5f164680b902d0ebd62dfa6f4bf3feffef
Dinesh Atri <datri@ex2india.com>
2021-07-20 14:13:51 +0530

    commit sorting

--- src/app/views/employee/employee.component.ts
+++ src/app/views/employee/employee.component.ts
@@ -13,16 +13,34 @@ import { selectEmployees } from 'src/app/state/employee.selectors';
 export class EmployeeComponent implements OnInit {
   employee = {} as Employee;
   employees: Employee[] = [];
+  isAscendingSort: boolean = true;
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
     this.store.pipe(select(selectEmployees)).subscribe(
       (res) => {
-        this.employees = res;
+        let arr: Employee[] = [];
+        arr = arr.concat(res);
+        this.employees = this.sortByKey(arr, this.isAscendingSort)
       },
       (err) => console.log(err),
       () => console.log('done!')
     );
+  }
+
+  sortData(){
+    this.isAscendingSort = !this.isAscendingSort;
+    let arr: Employee[] = [];
+    arr = arr.concat(this.employees);
+    this.employees = this.sortByKey(arr, this.isAscendingSort)
+  }
+
+  sortByKey(array: Employee[], isAcending: boolean) {
+    if (this.isAscendingSort) {
+      return array.sort((a, b) => a.workLocation < b.workLocation ? -1 : 1);
+    } else {
+      return array.sort((a, b) => a.workLocation < b.workLocation ? 1 : -1);
+    }
   }
 
 }
